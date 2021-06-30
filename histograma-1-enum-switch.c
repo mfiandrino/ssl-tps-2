7656269f1db297fd5ec63192c2e14c437a420f2f
nmartinezgross <ngross@frba.utn.edu.ar>
2021-06-30 15:34:25 -0300

    Agregación de lógica a implementación 1

--- histograma-1-enum-switch.c
+++ histograma-1-enum-switch.c
@@ -1,4 +1,5 @@
 #include "histograma.h"
+#include <stdio.h>
 
 unsigned int *histograma1()
 {
@@ -6,3 +7,51 @@ unsigned int *histograma1()
     printf("\nHas entrado a la implementacion con enum y switch\n");
     return contadores;
 }
+
+typedef enum {
+    OUT /* fuera de una palabra*/, 
+    IN /* dentro de una palabra*/
+}STATUS;
+
+
+void main()
+{
+    STATUS state = OUT;
+    //unsigned nw[100];
+    int c, nc;
+    nc = 0;
+    while ((c=getchar( ))!= EOF) {
+        //FILE *fp = fopen("text.txt","w");
+        switch (state){
+            case OUT:
+                switch (c){
+                    case ' ': case '\t': case '\n':
+                        state=OUT;
+                    break;
+                    default:
+                        ++nc;
+                        state = IN;
+                        printf("nc1 : %d ", nc, "\n");
+                    break;
+                }
+            break;
+            default: /* IN*/
+                switch (c){
+                    case ' ': case '\t': case '\n':
+                        state = OUT;
+                        //++nw[nc];
+                        //fputs(nw[nc], fp);
+                        printf("nc2 : %d ", nc, "\n");
+                        //printf("nw[nc]: %d ", nw[nc], "\n");                        
+                        nc = 0;
+                    break;
+                    default:
+                        ++nc;
+                        state = IN;
+                    break;
+                }
+       }
+       //fclose(fp);
+    }
+}
+/* Falta guardar los datos en archivo txt y ver el tema de los arrays. */
