4616f121e8eba0adde855ef51cca2a6171821d6c
nmartinezgross <ngross@frba.utn.edu.ar>
2021-07-04 16:17:15 -0300

    Modificacion implementacion 1

--- 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-1-enum-switch.c
+++ 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-1-enum-switch.c
@@ -1,6 +1,7 @@
 #include "histograma.h"
+#include <string.h>
 #include <stdio.h>
-
+#include <stdbool.h>
 
 unsigned int *histograma1()
 {
@@ -14,11 +15,10 @@ unsigned int *histograma1()
     return contadores;
 
     STATUS state = OUT;
-    //unsigned nw[100];
+    static unsigned conts[100] = {0};
     int c, nc;
     nc = 0;
     while ((c=getchar( ))!= EOF) {
-        //FILE *fp = fopen("text.txt","w");
         switch (state){
             case OUT:
                 switch (c){
@@ -28,7 +28,6 @@ unsigned int *histograma1()
                     default:
                         ++nc;
                         state = IN;
-                        printf("nc1 : %d ", nc, "\n");
                     break;
                 }
             break;
@@ -36,10 +35,8 @@ unsigned int *histograma1()
                 switch (c){
                     case ' ': case '\t': case '\n':
                         state = OUT;
-                        //++nw[nc];
-                        //fputs(nw[nc], fp);
-                        printf("nc2 : %d ", nc, "\n");
-                        //printf("nw[nc]: %d ", nw[nc], "\n");                        
+                        ++conts[nc];
+                        printf("palabra[CantCaracteres]: %d | Cantidad de caracteres: %d\n ", conts[nc],nc);                        
                         nc = 0;
                     break;
                     default:
@@ -48,6 +45,54 @@ unsigned int *histograma1()
                     break;
                 }
        }
-       //fclose(fp);
     }
+    return conts;
+}
+
+    typedef enum {
+        OUT /* fuera de una palabra*/, 
+        IN /* dentro de una palabra*/
+    }STATUS;
+
+int main(){
+
+
+    static unsigned int contadores[10] = {0,1,2,3,5,4,3,2,1,0};
+    printf("\nHas entrado a la implementacion con enum y switch\n");
+    
+
+    STATUS state = OUT;
+    static unsigned conts[100] = {0};
+    int c, nc;
+    nc = 0;
+    while ((c=getchar( ))!= EOF) {
+        switch (state){
+            case OUT:
+                switch (c){
+                    case ' ': case '\t': case '\n':
+                        state=OUT;
+                    break;
+                    default:
+                        ++nc;
+                        state = IN;
+                    break;
+                }
+            break;
+            default: /* IN*/
+                switch (c){
+                    case ' ': case '\t': case '\n':
+                        state = OUT;
+                        ++conts[nc];
+                        printf("palabra[CantCaracteres]: %d | Cantidad de caracteres: %d\n ", conts[nc],nc);                        
+                        nc = 0;
+                    break;
+                    default:
+                        ++nc;
+                        state = IN;
+                    break;
+                }
+       }
+    }
+    for(int i=0;i<100;i++)
+    printf("%d\n",conts[i]);
 }
