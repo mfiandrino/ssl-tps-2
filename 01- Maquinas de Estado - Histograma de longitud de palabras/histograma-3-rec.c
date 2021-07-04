4205e25cd338e6765b0204767cd488e46adfd4bb
tgomez00 <tgomez@frba.utn.edu.ar>
2021-07-04 19:33:24 -0300

    Update Primer Acercamiento

--- 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-3-rec.c
+++ 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-3-rec.c
@@ -1,8 +1,39 @@
+
 #include "histograma.h"
+#include <string.h>
+#include <stdio.h>
+
+int in( int caracter, int contador, unsigned int* nw )
+{
+     if (caracter == ' ' || caracter == '\n' || caracter == '\t') 
+     {
+           
+     }
+     else
+     {
+         printf("%c",caracter);
+         contador = in (caracter = getchar(), contador,nw) + 1; 
+     }
+
+     return contador;
+}
 
 unsigned int *histograma3()
 {
-    static unsigned int contadores[10] = {0,3,7,5,1};
     printf("\nHas entrado a la implementacion recursiva\n");
-    return contadores;
+
+    int caracter,nc,state = 0;
+    unsigned int nw[100]= {0};
+
+    caracter = getchar();
+      if ((caracter == ' ' || caracter == '\n' || caracter == '\t'))
+        {     
+        }
+        else
+        {
+            nc = in(caracter,nc, nw);
+            printf("%d\n", nc);
+        }  
+         
+    return nw;
 }
