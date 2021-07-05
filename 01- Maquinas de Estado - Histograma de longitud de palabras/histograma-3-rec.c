c54044453c0bad07c0e43b1be5e7981cda58129b
ivan3256 <ivan.roldan3256@gmail.com>
2021-07-05 15:01:42 -0300

    Update histograma-3-rec.c

--- 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-3-rec.c
+++ 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-3-rec.c
@@ -5,19 +5,25 @@
 
 int in( int caracter, int contador, unsigned int* nw )
 {
-     if (caracter == ' ' || caracter == '\n' || caracter == '\t') 
+     if (caracter == ' ' || caracter == '\n' || caracter == '\t')
      {
-           
+         out( caracter,nw, contador);
      }
      else
      {
          printf("%c",caracter);
-         contador = in (caracter = getchar(), contador,nw) + 1; 
+         contador = in (caracter = getchar(), contador,nw) + 1;
      }
 
      return contador;
 }
-
+void out( int caracter, unsigned int* nw, int contadorCaracter )
+{
+    ++nw[contadorCaracter];
+    printf("%u\n", nw[contadorCaracter]);
+    contadorCaracter = 0;
+    return nw;
+}
 unsigned int *histograma3()
 {
     printf("\nHas entrado a la implementacion recursiva\n");
@@ -27,13 +33,13 @@ unsigned int *histograma3()
 
     caracter = getchar();
       if ((caracter == ' ' || caracter == '\n' || caracter == '\t'))
-        {     
+        {
         }
         else
         {
             nc = in(caracter,nc, nw);
             printf("%d\n", nc);
-        }  
-         
+        }
+
     return nw;
 }
