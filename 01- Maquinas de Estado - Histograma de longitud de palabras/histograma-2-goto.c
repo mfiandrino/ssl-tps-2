129829b5d6e4f2b050e7670296ebda8bc305e3bc
alobianco <agustinlobianco@frba.utn.edu.ar>
2021-07-10 20:35:25 -0300

    Cambio en Histograma 2 GOTO

--- 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-2-goto.c
+++ 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-2-goto.c
@@ -1,43 +1,29 @@
 #include "histograma.h"
 #include <string.h>
 #include <stdio.h>
-#include <stdbool.h>
-#define IN 1
-#define OUT 0
 
 unsigned int *histograma2(unsigned int* contadores, unsigned int cantContadores)
 //unsigned int *histograma2()
 {
     printf("\nHas entrado a la implementacion con goto\n");
-    int c,nc,state;
-    c = nc = 0;
-    bool primeraLectura = true;
-    state = OUT;
-    //static unsigned int nw[largoMaxPalabra] = {0};
-    while ((c=getchar())!=EOF){
-        if ((c == ' ' || c == '\n' || c == '\t')||(state == OUT)){
-            goto fueraDePalabra;
-        }
-        else{
-            goto dentroDePalabra;
-        }
-        fueraDePalabra:
-            if (primeraLectura == false && nc >= 1){
-                if(nc >= cantContadores)
-                    nc = --cantContadores;
-                ++contadores[nc];
-                //printf("palabra[CantCaracteres]: %d | Cantidad de caracteres: %d\n ", nw[nc],nc);
-                nc = 0;
-                state = OUT;
-                
-            }
-            primeraLectura = false;
-        dentroDePalabra:
-            if (c != ' ' && c != '\n' && c != '\t'){
-            //printf ("c:%c\n",c);
-            ++nc;
-            state = IN;
-            }
-    }
-    return contadores;
+    int c;
+    c = cantContadores = 0;
+    c = getchar();
+    if (c == EOF) return contadores;
+    else if (c == ' ' || c == '\n' || c == '\t') goto OUT;
+    else goto IN;
+    OUT:
+        ++contadores[cantContadores];
+        cantContadores = 0;
+        c = getchar ();
+        if (c == EOF) return contadores;
+        else if (c == ' ' || c == '\n' || c == '\t') goto OUT;
+        else goto IN;
+    IN:
+        ++cantContadores;
+        c = getchar ();
+        if (c == EOF) return contadores;
+        else if (c == ' ' || c == '\n' || c == '\t') goto OUT;
+        else goto IN;
+        
 }
