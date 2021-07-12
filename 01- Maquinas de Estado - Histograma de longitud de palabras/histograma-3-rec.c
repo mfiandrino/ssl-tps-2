eaca31c47db73f03f9c6dce970a2762846e58fab
tgomez00 <tgomez@frba.utn.edu.ar>
2021-07-11 21:22:47 -0300

    Agregamos la variable cantContadores en funcion IN y OUT

--- 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-3-rec.c
+++ 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-3-rec.c
@@ -3,47 +3,49 @@
 #include <string.h>
 #include <stdio.h>
 
-static void out(int, int, unsigned int*);
-static void in(int, int, unsigned int*);
+static void out(int, int, unsigned int*, int);
+static void in(int, int, unsigned int*,int);
 
-void in( int caracter, int cantContadores, unsigned int* contadores )
+void in( int caracter, int nc, unsigned int* contadores, int cantContadores )
 {
      if (caracter == ' ' || caracter == '\n' || caracter == '\t' || caracter == EOF)
      {
-         out( caracter, cantContadores,contadores);
+         out( caracter, nc,contadores, cantContadores);
      }
      else
      {
-         ++cantContadores;
-         in ( caracter = getchar(), cantContadores,contadores);
+         ++nc;
+         in ( getchar(), nc,contadores, cantContadores);
      }
 
 }
-void out( int caracter, int cantContadores, unsigned int* contadores)
+void out( int caracter, int nc, unsigned int* contadores, int cantContadores)
 {
     if(caracter!=EOF)
     {
-        ++contadores[cantContadores]; 
-        cantContadores = 0; 
+        if(nc >= cantContadores)
+        nc = --cantContadores;
 
-        in( caracter = getchar(),cantContadores,contadores);
+        ++contadores[nc]; 
+        nc = 0; 
+
+        in( getchar(),nc,contadores, cantContadores);
     }
 }
 void histograma3(unsigned int* contadores, unsigned int cantContadores)
 {
     printf("\nHas entrado a la implementacion recursiva\n");
 
-    int caracter = 0;
-    int nc=0;
+    int caracter,nc = 0;
     caracter = getchar();
 
       if ((caracter == ' ' || caracter == '\n' || caracter == '\t' || caracter == EOF))
         {
-            out(caracter, nc ,contadores);
+            out(caracter, nc ,contadores, cantContadores);
         }
         else
         {
-            in(caracter,nc ,contadores);
+            in(caracter,nc ,contadores,cantContadores);
         }   
 
     for(int c = 0;c<10;c++) // Puse esto para comprobar que al final lleguen los valores de nw
