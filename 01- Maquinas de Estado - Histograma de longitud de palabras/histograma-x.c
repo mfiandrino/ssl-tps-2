3bdd9981e7ed0f67eef4186b6739ba58018e26b3
alobianco <agustinlobianco@frba.utn.edu.ar>
2021-07-11 23:20:36 -0300

    Implementacion x

--- 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-x.c
+++ 01- Maquinas de Estado - Histograma de longitud de palabras/histograma-x.c
@@ -2,37 +2,24 @@
 #include "histograma.h"
 #include <string.h>
 #include <stdio.h>
-#include <stdbool.h>
-#define IN 1
-#define OUT 0
 
 void histograma4(unsigned int* contadores,unsigned int cantContadores)
 {
-    printf("\nHas entrado a la implementacion con goto\n");
-    int c,nc,state;
+    printf("\nHas entrado a la implementacion x\n");
+    int c,nc;
     c = nc = 0;
-    bool primeraLectura = true;
-    state = OUT;
-    //static unsigned int contadores[largoMaxPalabra] = {0};
     while ((c=getchar())!=EOF){
-        if ((c == ' ' || c == '\n' || c == '\t')||(state == OUT)){
-            goto fueraDePalabra;
-        }
-        else{
-            goto dentroDePalabra;
-        }
-        fueraDePalabra:
-            if (primeraLectura == false && nc >= 1){
-                ++contadores[nc];
-                nc = 0;
-                state = OUT;
-                
-            }
-            primeraLectura = false;
-        dentroDePalabra:
+        if ((c == ' ' || c == '\n' || c == '\t')) goto OUT;
+        else goto IN;
+        
+        OUT:
+            if(nc >= cantContadores)
+                nc = --cantContadores;
+            ++contadores[nc];
+            nc = 0;
+        IN:
             if (c != ' ' && c != '\n' && c != '\t'){
             ++nc;
-            state = IN;
             }
     }
     return;
