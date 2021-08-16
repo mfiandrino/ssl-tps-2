ef99438addd4a4c4b0d95c068ca1eea991922d42
tgomez00 <tgomez@frba.utn.edu.ar>
2021-08-16 14:34:43 -0300

    Avance (Descarta el comentario pero deja algun parentesis o asterisco escrito)

--- 04 -/comentarios.c
+++ 04 -/comentarios.c
@@ -10,46 +10,54 @@ int main(void){
     int prevC;
     int c;
     prevC = getchar ();
-    while ((c=getchar())!=EOF){
-    if (prevC == '/' && c == '*') goto IN1;
-    if (prevC == '/' && c == '/') goto IN2;
-    else goto OUT;
+    while ((c=getchar())!=EOF)
+    {
+        if ((prevC == '/') && (c == '*')) goto IN1;
+        //if ((prevC == '/') && (c == '/')) goto IN2;
+        else goto OUT;
 
-    OUT:
-    putchar(prevC);
-    prevC = c;
+        OUT:
+        putchar(prevC);
+        prevC = c;
 
-    IN1:
-    printf("previo if \n");
-    if (prevC != '*' && c != '/'){
-        printf("adentro1 \n");
+        IN1:
+        if ((prevC == '/') && (c == '*'))
+        {
+            prevC = getchar();
+            c = getchar();
+        }
+        //printf("previo if \n");
+        while ( (prevC != '*') && (c != '/'))
+        {
+            prevC = c;
+            c = getchar();
+        }
+        if ( (prevC == '*') && (c == '/'))
+        {
+            putchar(' ');
+            prevC = c;
+        }
+        /*
+        IN2:
+        //if (c == '\n'){
+        //    putchar(' ');
+        //    c = getchar();
+            //prevC = c;
+        //    goto OUT;}
+        //else prevC = c;
+        while (c != '\n')
+        {
+            printf("adentro1");
+            prevC = c;
+            printf("adentro2");
+            c = getchar();
+            printf("adentro3");
+        }
+        printf("salio");
+        putchar(' ');
         prevC = c;
-        printf("adentro2 \n");
-        c = getchar();
-        printf("adentro3 \n");
-        goto IN1;}
-    printf("post if \n");
-    putchar(' ');
-    prevC = c;
-    //goto OUT;
-    
-    IN2:
-    //if (c == '\n'){
-    //    putchar(' ');
-    //    c = getchar();
-        //prevC = c;
-    //    goto OUT;}
-    //else prevC = c;
-    while (c != '\n'){
-        printf("adentro1");
-        prevC = c;
-        printf("adentro2");
-        c = getchar();
-        printf("adentro3");}
-    printf("salio");
-    putchar(' ');
-    prevC = c;
-    //goto OUT;
+        //goto OUT;
+        */
 
     }
     
