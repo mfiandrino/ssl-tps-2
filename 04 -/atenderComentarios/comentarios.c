e0a25833f78d86c18102a86bd6f2711098441f32
tgomez00 <tgomez@frba.utn.edu.ar>
2021-08-22 20:48:48 -0300

    Actualizacion tabulador

--- 04 -/atenderComentarios/comentarios.c
+++ 04 -/atenderComentarios/comentarios.c
@@ -14,73 +14,79 @@ typedef enum {
     COMMENT2 /* dentro de un comentario con /* */,
 }state;
 
-int main(void){
-state s = OUT;
-int prevC, c;
-prevC = getchar ();
-while ((c=getchar())!=EOF){
-    switch (s){
-
-    case OUT:
-        switch (prevC){
-        case '/': //posible comentario
-            switch (c){
-                case '/': //comentario del tipo '//'
-                    s= COMMENT1;
-                    break;
-                case '*': //comentario del tipo '/*'
-                    s = COMMENT2;
-                break;
+int main(void)
+{
+    state s = OUT;
+    int prevC, c;
+    prevC = getchar ();
+    while ((c=getchar())!=EOF)
+    {
+        switch (s){
+        case OUT:
+            switch (prevC)
+            {
+            case '/': //posible comentario
+                switch (c)
+                {
+                    case '/': //comentario del tipo '//'
+                        s= COMMENT1;
+                        break;
+                    case '*': //comentario del tipo '/*'
+                        s = COMMENT2;
+                        break;
                 default: //no es comentario
                     putchar(prevC);
                     prevC=c;
                     s=OUT;
                 break;
+                }
+            break;
+            default: //no es comentario (prevC \= '/')
+                putchar(prevC);
+                prevC= c;
+                s = OUT;
+            break;
             }
         break;
-        default: //no es comentario (prevC \= '/')
-            putchar(prevC);
-            prevC= c;
-            s = OUT;
+/*--------------------------------------------------------------------------------------------------------*/
+        case COMMENT1: //comentario del tipo '//'
+            switch (prevC)
+            {
+                case '\n': //concluye el comentario
+                    putchar(' ');
+                    putchar(prevC);
+                    prevC = c;
+                    s=OUT;
+                    break;
+                default: //continua el comentario (prevC \= '\n')
+                    prevC=c;
+                    s = COMMENT1;
+            break;
+            }
         break;
-        }
-    break;
-
-    case COMMENT1: //comentario del tipo '//'
-        switch (prevC){
-        case '\n': //concluye el comentario
-            putchar(' ');
-            putchar(prevC);
-            prevC = c;
-            s=OUT;
-        break;
-        default: //continua el comentario (prevC \= '\n')
-            prevC=c;
-            s = COMMENT1;
-        break;
-        }
-    break;
-
-    default: //comentario del tipo '/*'
-        switch (prevC){
-        case '*': //posible cierre de comentario
-            switch (c){
-                case '/': //concluye el comentario
-                putchar(' ');
-                prevC =' ';
-                s=OUT;
+/*--------------------------------------------------------------------------------------------------------*/
+        default: //comentario del tipo '/*'
+            switch (prevC)
+            {
+            case '*': //posible cierre de comentario
+                switch (c)
+                {
+                    case '/': //concluye el comentario
+                        putchar(' ');
+                        prevC =' ';
+                        s=OUT;
+                        break;
+                    default: //continua el comentario (c \= '/')
+                        prevC=c;
+                        s = COMMENT2;
                 break;
-                default: //continua el comentario (c \= '/')
+                }
+            break;
+            default: //continua el comentario (prevC \= '*')
                 prevC=c;
                 s = COMMENT2;
-                break;
+            break;
             }
-        break;
-        default: //continua el comentario (prevC \= '*')
-            prevC=c;
-            s = COMMENT2;
-        break;
         }
     }
-}
 }
