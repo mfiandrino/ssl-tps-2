ff0f64a0f7061cfaa618e33bf402d4ddbfef29ab
nmartinezgross <ngross@frba.utn.edu.ar>
2021-08-22 11:50:37 -0300

    Implementacion de reemplazo de comentarios

--- 04 -/comentarios.c
+++ 04 -/comentarios.c
@@ -3,64 +3,82 @@ Reemplazar comentarios por espacios
 Grupo Nro 3 del curso K2051 Lunes TN
 20210712
 */
+
+
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+typedef enum {
+    OUT /* fuera de una palabra*/, 
+    IN /* dentro de una palabra*/,
+    IN2,
+}state;
 
 int main(void){
-    int prevC;
-    int c;
-    prevC = getchar ();
-    while ((c=getchar())!=EOF)
-    {
-        if ((prevC == '/') && (c == '*')) goto IN1;
-        //if ((prevC == '/') && (c == '/')) goto IN2;
-        else goto OUT;
-
-        OUT:
+state s = OUT;
+int prevC, c;
+prevC = getchar ();
+while ((c=getchar())!=EOF){
+    switch (s){
+    case OUT:
+        switch (prevC){
+        case '/':
+            switch (c){
+                case '/':
+                s= IN;
+                break;
+                case '*':
+                s = IN2;
+                break;
+                default:
+                putchar(prevC);
+                prevC=c;
+                s=OUT;
+                break;
+            }
+        break;
+        default: //(prevC /= '/')
+        putchar(prevC);
+        prevC= c;
+        s = OUT;
+        break;
+        }
+    break;
+    case IN:
+        switch (prevC){
+        case '\n':
+        putchar(' ');
         putchar(prevC);
         prevC = c;
-
-        IN1:
-        if ((prevC == '/') && (c == '*'))
-        {
-            prevC = getchar();
-            c = getchar();
+        s=OUT;
+        break;
+        default: //(prevC /= '\n')
+        prevC=c;
+        s = IN;
+        break;
         }
-        //printf("previo if \n");
-        while ( (prevC != '*') && (c != '/'))
-        {
-            prevC = c;
-            c = getchar();
+    break;
+    default:
+        switch (prevC){
+        case '*':
+            switch (c){
+                case '/':
+                putchar(' ');
+                prevC =' ';
+                s=OUT;
+                break;
+                default:
+                prevC=c;
+                s = IN2;
+                break;
+            }
+        break;
+        default:
+        prevC=c;
+        s = IN2;
+        break;
         }
-        if ( (prevC == '*') && (c == '/'))
-        {
-            putchar(' ');
-            prevC = c;
-        }
-        /*
-        IN2:
-        //if (c == '\n'){
-        //    putchar(' ');
-        //    c = getchar();
-            //prevC = c;
-        //    goto OUT;}
-        //else prevC = c;
-        while (c != '\n')
-        {
-            printf("adentro1");
-            prevC = c;
-            printf("adentro2");
-            c = getchar();
-            printf("adentro3");
-        }
-        printf("salio");
-        putchar(' ');
-        prevC = c;
-        //goto OUT;
-        */
-
     }
-    
-    //putchar(prevC); 
-
+}
 }
