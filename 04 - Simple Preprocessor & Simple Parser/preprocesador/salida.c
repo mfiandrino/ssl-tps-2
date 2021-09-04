ed031216f267661865e61994b5200826770d5d14
Maximiliano Fiandrino <maxi.fiandrino@gmail.com>
2021-09-04 19:48:10 -0300

    Ordenamiento de carpetas

---
+++ 04 - Simple Preprocessor & Simple Parser/preprocesador/salida.c
@@ -0,0 +1,17 @@
+#include "funcionesPreprocesador.h"
+#define UNO "DOS"
+#define TRES "CUATRO"
+#define CINCO "SEIS"
+
+/*
+#include algo.h
+*/
+int main(TRES)
+{
+    'hola/*chau*/qeut//hoal'
+    int resultado CINCO = preprocesador(); //asd
+    /*
+    hola
+    */
+    return UNO;
+}
